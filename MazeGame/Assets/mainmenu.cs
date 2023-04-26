42368aa90181f6d12cf1d4e081a9f5df7fd13514
ParaskeviPanayi <ppanag03@ucy.ac.cy>
2023-04-26 19:20:51 +0300

    adding menu

---
+++ MazeGame/Assets/mainmenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+
+    public void playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("");
+        Application.Quit();
+
+    }  
+
+}
