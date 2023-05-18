921f78f77bdb1569fe045b9d754ab3718ab235c3
eeliad0 <eeliad02@ucy.ac.cy>
2023-05-18 21:52:04 +0300

    finished product!

--- MazeGame/Assets/Scripts/Physics.cs
+++ MazeGame/Assets/Scripts/Physics.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine; 
 
 public class Physics : MonoBehaviour
@@ -8,13 +9,14 @@ public class Physics : MonoBehaviour
 
     [SerializeField] private bool useOnCollision;
     private Rigidbody rigidbody;
+    public GameObject Image;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
 
-    void OnCollisionEnter(Collision collision){
+    async void OnCollisionEnter(Collision collision){
         if(!useOnCollision){
             return;
         }
@@ -31,17 +33,22 @@ public class Physics : MonoBehaviour
             ParticleSystem part = child1.GetComponent<ParticleSystem>();
             part.Play();
             Destroy(collision.gameObject,0.5f);
-
-
-    }
+            
+        }
     if(collision.gameObject.tag == StringRepo.PrincessDoorTag && gm.FlowerCheck()){
             FindObjectOfType<AudioManager>().Play("doorbreak");
             GameObject child1 = collision.gameObject.transform.GetChild(0).gameObject;
             ParticleSystem part = child1.GetComponent<ParticleSystem>();
             part.Play();
             Destroy(collision.gameObject,0.5f);
-            
+            StartCoroutine(EnableGameoverPanel());
 
     }
   }
+  IEnumerator EnableGameoverPanel()
+    {
+        yield return new WaitForSeconds(5);
+ 
+        Image.SetActive(true);
+    }
 }
--- MazeGame/Assets/Scripts/timer.cs
+++ MazeGame/Assets/Scripts/timer.cs
@@ -9,23 +9,31 @@ public class timer : MonoBehaviour
 
     public Text timerText;
     private float starTime;
+    public string seconds;
+    public string minutes;
 
     // Start is called before the first frame update
     void Start()
     {
         starTime = Time.time;
+    
     }
 
     // Update is called once per frame
     void Update()
     {
         float t = Time.time - starTime;
-        string minutes = ((int)t / 60).ToString();
-        string seconds = (t % 60).ToString("f0");
+        minutes = ((int)t / 60).ToString();
+        seconds = (t % 60).ToString("f0");
 
         //Debug.Log(seconds);
 
-        timerText.text=minutes+":"+seconds;
+        timerText.text = minutes+":"+seconds;
 
+    }
+
+    public string stop(){
+        return minutes+":"+seconds;
+        
     }
 }
